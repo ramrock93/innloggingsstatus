19dfdde6bb6b2b42a0ac584de9d50d4849ea02fe
Christopher Santana <christopher.santana@nav.no>
2020-09-02 12:54:02 +0200

    Omdøp authLevel til securityLevel i UserInfo og gjør type til String.

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/auth/UserInfo.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/auth/UserInfo.kt
@@ -3,10 +3,10 @@ package no.nav.personbruker.innloggingsstatus.auth
 data class UserInfo private constructor(
     val authenticated: Boolean,
     val name: String?,
-    val authLevel: Int?
+    val securityLevel: String?
 ) {
     companion object {
-        fun authenticated(name: String, authLevel: Int): UserInfo = UserInfo(true, name, authLevel)
+        fun authenticated(name: String, authLevel: Int): UserInfo = UserInfo(true, name, authLevel.toString())
         fun unAuthenticated(): UserInfo = UserInfo(false, null, null)
     }
 }
