03652867acd2d27fb2dd7847270829778b2f85ac
Christopher Santana <christopher.santana@nav.no>
2020-09-02 15:01:25 +0200

    Forbedret logging. Fikset feil i jwt parser.

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/pdl/PdlService.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/pdl/PdlService.kt
@@ -1,10 +1,12 @@
 package no.nav.personbruker.innloggingsstatus.pdl
 
 import no.nav.personbruker.innloggingsstatus.pdl.query.PdlNavn
-import no.nav.personbruker.innloggingsstatus.sts.STSConsumer
 import no.nav.personbruker.innloggingsstatus.sts.StsService
+import org.slf4j.LoggerFactory
 
 class PdlService(private val pdlConsumer: PdlConsumer, private val stsService: StsService) {
+
+    val log = LoggerFactory.getLogger(PdlService::class.java)
 
     suspend fun getSubjectName(ident: String): PdlNavn? {
         return try {
@@ -14,8 +16,10 @@ class PdlService(private val pdlConsumer: PdlConsumer, private val stsService: S
             }
         } catch (e: PdlAuthenticationException) {
             stsService.invalidateToken()
+            log.info("Invalidating sts token")
             null
         } catch (e: Exception) {
+            log.warn("Pdl-related exception.", e)
             null
         }
     }
--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/sts/jwt/JwtTokenDecoder.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/sts/jwt/JwtTokenDecoder.kt
@@ -6,22 +6,30 @@ import com.fasterxml.jackson.databind.JsonMappingException
 import no.nav.personbruker.innloggingsstatus.common.fromBase64
 import no.nav.personbruker.innloggingsstatus.common.readObject
 import no.nav.personbruker.innloggingsstatus.config.JsonDeserialize.objectMapper
+import org.slf4j.LoggerFactory
 import java.lang.Exception
 import java.time.Instant
 
 object JwtTokenDecoder {
 
+    val log = LoggerFactory.getLogger(JwtTokenDecoder::class.java)
+
     fun decodeExpiryTime(jwtToken: String): Instant {
         return try {
-            val payloadString = jwtToken.split("\\.")
+            val payloadString = jwtToken.split(".")
                 .let { segments -> segments[1].fromBase64()}
 
             val payload: JwtPayload = objectMapper.readObject(payloadString)
             Instant.ofEpochSecond(payload.expiryTime)
         } catch (e: JsonMappingException) {
+            log.warn("Error parsing expiry date from jwt token.")
             throw Exception("Error parsing expiry date from jwt token.", e)
         } catch (e: JsonParseException) {
+            log.warn("Invalid json found in jwt token.")
             throw Exception("Invalid json found in jwt token.", e)
+        } catch (e: Exception) {
+            log.warn("Exception thrown while parsing jwt expiry time.")
+            throw Exception("Exception thrown while parsing jwt expiry time.", e)
         }
     }
 }
