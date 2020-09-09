c3073b17ed815bd3691ba6fa3db58b67b90959bb
Christopher Santana <christopher.santana@nav.no>
2020-09-09 11:51:15 +0200

    Endret serialisering av datoer.

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/config/bootstrap.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/config/bootstrap.kt
@@ -1,5 +1,6 @@
 package no.nav.personbruker.innloggingsstatus.config
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.application.Application
 import io.ktor.application.install
@@ -38,6 +39,7 @@ fun Application.mainModule() {
     install(ContentNegotiation) {
         jackson {
             registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
     }
 
