26892595976b938db3ec323de9b682cf4700e4e9
Christopher Santana <christopher.santana@nav.no>
2020-09-15 10:31:37 +0200

    Byttet v2 tilbake til v1 ettersom konsekvensene av feil mapping ikke var så store som fryktet.

--- src/main/kotlin/no/nav/personbruker/innloggingsstatus/common/metrics/influx/metricNames.kt
+++ src/main/kotlin/no/nav/personbruker/innloggingsstatus/common/metrics/influx/metricNames.kt
@@ -1,5 +1,5 @@
 package no.nav.personbruker.innloggingsstatus.common.metrics.influx
 
-private const val METRIC_NAMESPACE = "personbruker.innloggingsstatus.v2"
+private const val METRIC_NAMESPACE = "personbruker.innloggingsstatus.v1"
 
 const val USER_AUTH_INFO = "$METRIC_NAMESPACE.userAuthInfo"
